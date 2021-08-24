1498913b6f3ccda51c066f5c20be1c9456f3924f
Carlos G. del Rosal <carlosglzmx@gmail.com>
2021-08-24 16:05:11 -0700

    Initial Commit

---
+++ MARCO_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MARCO_App
+//
+//  Created by Carlos del Rosal on 24/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
