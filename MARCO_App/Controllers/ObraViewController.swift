99c7f1f84de71f67e60dd11d7521cdccb418bddb
Alexia <a01283646@itesm.mx>
2021-09-07 21:02:43 -0500

    View de la obra

---
+++ MARCO_App/Controllers/ObraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ObraViewController.swift
+//  MARCO_App
+//
+//  Created by Alexia Fernanda Saucedo Romero on 07/09/21.
+//
+
+import UIKit
+
+class ObraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
