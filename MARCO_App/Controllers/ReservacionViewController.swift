de55009e0cadb09cd792e631704563e676fd532c
abab <aaa@aa.com>
2021-09-08 19:46:10 -0700

    Constraints agregados / pantalla agendar citas agregada / navigation controller agregado para presentaciones

---
+++ MARCO_App/Controllers/ReservacionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReservacionViewController.swift
+//  MARCO_App
+//
+//  Created by user195584 on 9/8/21.
+//  Copyright © 2021 com.tec.tc2007b. All rights reserved.
+//
+
+import UIKit
+
+class ReservacionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Agendar Visitas"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
