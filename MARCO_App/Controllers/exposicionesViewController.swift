8eaa4724d22a14fafc516b7b7e227750b12933ae
Alexia Fernanda Saucedo Romero <72261496+AlexiaSaucedo@users.noreply.github.com>
2021-10-05 23:44:54 -0500

    Collection view Cell
    En vez de ser botones es un Collection View, en exposiciones

--- MARCO_App/Controllers/exposicionesViewController.swift
+++ MARCO_App/Controllers/exposicionesViewController.swift
@@ -7,18 +7,25 @@
 
 import UIKit
 
-class exposicionesViewController: UIViewController {
+class exposicionesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var exposiciones: [Exposicion] = []
+    
+    @IBOutlet weak var obrasGrid: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Exposiciones"
-        
+
+        obrasGrid.delegate = self
+        obrasGrid.dataSource = self
         
         fetch()
-        // Do any additional setup after loading the view.
+        
+        let nib = UINib(nibName: "ObraCollectionViewCell", bundle: nil)
+        
+        obrasGrid.register(nib, forCellWithReuseIdentifier: "obraCollectionCell")
     }
     
     func fetch(){
@@ -36,15 +43,26 @@ class exposicionesViewController: UIViewController {
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        exposiciones.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "obraCollectionCell", for: indexPath) as! ObraCollectionViewCell
+        
+        let obra = exposiciones[indexPath.row]
+        let name = obra.title
+        cell.obraTitle = name
+        
+//        let url = URL(string: "")!
+//        cell.obraImage.downloaded(from: url)
+        cell.obraImage = UIImage(named: "Obra1")
+        
+        
+        return cell
+    }
 
+   
 }
---
+++ MARCO_App/Views/ObraCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ObraCollectionViewCell.swift
+//  MARCO_App
+//
+//  Created by Alexia Fernanda Saucedo Romero on 05/10/21.
+//  Copyright © 2021 com.tec.tc2007b. All rights reserved.
+//
+
+import UIKit
+
+class ObraCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var obraImage: UIImageView!
+    @IBOutlet weak var obraTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
