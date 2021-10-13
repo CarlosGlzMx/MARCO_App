f2cb16f38dea01bfd36a05403a498903592cf058
Alexia Fernanda Saucedo Romero <72261496+AlexiaSaucedo@users.noreply.github.com>
2021-10-13 09:57:28 -0500

    new model Evento

---
+++ MARCO_App/Controllers/EventoModel.swift
@@ -0,0 +1,15 @@
+//  EventoModel.swift
+//  MARCO_App
+//
+//  Created by Alumno on 13/10/21.
+//  Copyright © 2021 com.tec.tc2007b. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - EventoElement
+struct EventoElement {
+    let id: String
+    let events: [String]
+    let date: String
+}
